2d9fa9bcf8ad04defcde8071e5b4c6c090f53a32
Rupesh Raut <rupeshraut99396@gmail.com>
2024-12-05 18:51:42 +0530

    added ping API

--- backend/app.py
+++ backend/app.py
@@ -167,6 +167,11 @@ def download_log():
         return jsonify({"error": str(e)}), 500
 
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({"response": "pong"})
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
